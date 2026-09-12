befc2dc01276af56be03a503e315f68deb27ffa5
AllR1ght01 <pooio5770@gmail.com>
2026-09-12 16:58:36 +0700

    Delete vmpc directory

--- vmpc/render/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Custom incremental markdown rendering and status chrome."""
-
-from vmpc.render.markdown import MarkdownStreamCollector, render_markdown
-from vmpc.render.inline import render_inline
-
-__all__ = ["MarkdownStreamCollector", "render_markdown", "render_inline"]
